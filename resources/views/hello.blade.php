66ce0b9b85388eabaa9d35432c2241f07559ef35
Samir <120911119+samir2602@users.noreply.github.com>
2026-04-07 12:04:03 +0530

    Added Hello route in blade view

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello Page</title>
+</head>
+<body>
+    <h1>Hello, {{ $name1 }} & {{ $name2 }}! 👋</h1>
+    <p>I am building my first Laravel app!</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    $name1 = "Samir";
+    $name2 = "Khushi";
+    return view('hello', ['name1' => $name1, 'name2' => $name2]);
+});
